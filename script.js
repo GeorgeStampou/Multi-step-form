68e05dcd10915bcb142683cd5365a20bfc3df3e2
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-09-14 00:42:41 +0300

    one form, one submit, each step different div

--- script.js
+++ script.js
@@ -3,44 +3,31 @@ const form2 = document.getElementById("form2");
 const form3 = document.getElementById("form3");
 const next1 = document.getElementById("next1");
 const next2 = document.getElementById("next2");
-const back2 = document.getElementById("back2");
+const previous2 = document.getElementById("previous2");
 const finishButton = document.getElementById("finish-button");
-const back3 = document.getElementById("back3");
+const previous3 = document.getElementById("previous3");
+
 
 
 next1.addEventListener("click", function(){change(form1,form2,)});
-back2.addEventListener("click", function(){change(form2,form1)});
+previous2.addEventListener("click", function(){change(form2,form1)});
 next2.addEventListener("click", function(){change(form2,form3)});
-back3.addEventListener("click", function(){change(form3,form2)});
+previous3.addEventListener("click", function(){change(form3,form2)});
 finishButton.addEventListener("click",function(){finish(form3)});
 
 
 function change(firstElement, secondElement){
-    /*
-    if(validateForm(firstElement)){
+
+    const inputElements = [...firstElement.getElementsByTagName("input")];
+    const allValid = inputElements.every(input=> input.reportValidity());
+    if(allValid){
         firstElement.classList.remove("form-step-active");
         secondElement.classList.add("form-step-active");
-    }*/
-    /*firstElement.submit();*/
-    firstElement.classList.remove("form-step-active");
-    secondElement.classList.add("form-step-active");
+    }
+
 }
 
 function finish(form){
     /*form.submit();*/
     alert("FINISHED")
 }
-/*
-function validateForm(form){
-    const inputElements = form.getElementsByTagName("input");
-    let valid = false;
-    for (const element of inputElements){
-        if(element.value == ""){
-            valid = false;
-        } else{
-            valid = true;
-        }
-    }
-    return valid;
-}
-*/
