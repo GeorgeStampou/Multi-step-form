d9e4fcaa0b3eb611c3a6ab2e952a6762b6f295ab
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-09-13 17:07:54 +0300

    back button works, styling of buttons done

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const form1 = document.getElementById("form1");
 const form2 = document.getElementById("form2");
 const next1 = document.getElementById("next1");
 const next2 = document.getElementById("next2");
+const back2 = document.getElementById("back2");
 
 next1.onclick = function(){
     form1.classList.remove("form-step-active");
     form2.classList.add("form-step-active");
+}
+
+back2.onclick = function(){
+    form2.classList.remove("form-step-active");
+    form1.classList.add("form-step-active");
 }
