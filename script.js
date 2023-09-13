b828c3b0bcc61f61604e49190cb75b151f5e8083
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-09-13 18:41:19 +0300

    second form ok, buttons ok (i think)

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const form1 = document.getElementById("form1");
 const form2 = document.getElementById("form2");
+const form3 = document.getElementById("form3");
 const next1 = document.getElementById("next1");
 const next2 = document.getElementById("next2");
 const back2 = document.getElementById("back2");
+const finishButton = document.getElementById("finish-button");
+const back3 = document.getElementById("back3");
 
-next1.onclick = function(){
-    form1.classList.remove("form-step-active");
-    form2.classList.add("form-step-active");
+
+next1.addEventListener("click", function(){change(form1,form2)});
+back2.addEventListener("click", function(){change(form2,form1)});
+next2.addEventListener("click", function(){change(form2,form3)});
+back3.addEventListener("click", function(){change(form3,form2)});
+finishButton.addEventListener("click",function(){finish(form3)});
+
+
+function change(firstElement, secondElement){
+    firstElement.classList.remove("form-step-active");
+    firstElement.submit();
+    secondElement.classList.add("form-step-active");
 }
 
-back2.onclick = function(){
-    form2.classList.remove("form-step-active");
-    form1.classList.add("form-step-active");
+function finish(form){
+    form.submit();
+    alert("FINISHED")
 }
