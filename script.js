d73920088d89c44e77de6817d7d1f1641ab639ec
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-09-14 14:11:43 +0300

    added validation at script, minor changes css,html

--- script.js
+++ script.js
@@ -1,33 +1,84 @@
-const form1 = document.getElementById("form1");
-const form2 = document.getElementById("form2");
-const form3 = document.getElementById("form3");
-const next1 = document.getElementById("next1");
-const next2 = document.getElementById("next2");
-const previous2 = document.getElementById("previous2");
-const finishButton = document.getElementById("finish-button");
-const previous3 = document.getElementById("previous3");
+const multiForm = document.getElementById("form");
+const forms = [...multiForm.querySelectorAll("div.div-form")];
+console.log(forms);
 
+let formcounter = 0;
 
+multiForm.addEventListener("click", event =>{
 
-next1.addEventListener("click", function(){change(form1,form2,)});
-previous2.addEventListener("click", function(){change(form2,form1)});
-next2.addEventListener("click", function(){change(form2,form3)});
-previous3.addEventListener("click", function(){change(form3,form2)});
-finishButton.addEventListener("click",function(){finish(form3)});
+    const inputElements = [...forms[formcounter].querySelectorAll("input")];
+    
+    if(event.target.matches("[next]") && checkValidity(inputElements) && checkPass(inputElements)){
+        formcounter += 1;
+    } else if(event.target.matches("[previous]")){
+        formcounter -= 1;
+    } else if(event.target.matches("#finish-button") && checkValidity(forms)){
+        alert("FINISHED");
+        multiForm.submit();
+        
+    } else return
+    
+    updateForm(formcounter);
+})
 
-
-function change(firstElement, secondElement){
-
-    const inputElements = [...firstElement.getElementsByTagName("input")];
-    const allValid = inputElements.every(input=> input.reportValidity());
-    if(allValid){
-        firstElement.classList.remove("form-step-active");
-        secondElement.classList.add("form-step-active");
-    }
-
+function updateForm(counter){
+    forms.forEach(form => {
+        if(form.classList.contains("form-step-active")){
+            form.classList.remove("form-step-active");
+        }
+         forms[counter].classList.add("form-step-active");
+    });
 }
 
-function finish(form){
-    /*form.submit();*/
-    alert("FINISHED")
+function checkValidity(inputElements){
+    const allValid = inputElements.every(input => input.reportValidity());
+    
+    console.log(allValid);
+    return allValid;
+}
+
+function checkPass(inputElements){
+    
+    let count = 0;
+    let passwordValue = "";
+    let repeatPassValue = "a";
+    let repeatPassElement;
+    let index = 0;
+    for (const input of inputElements){
+        if(input.name === "password"){
+            passwordValue  = input.value;
+            count += 1;
+        } else if(input.name === "repeat-pass"){
+            repeatPassElement = input;
+            repeatPassValue = input.value;
+            count += 1;
+            index = inputElements.indexOf(input);
+        }
+    }
+
+    if (count == 0){
+        return true;
+    }
+    let valid = (passwordValue === repeatPassValue);
+    console.log(passwordValue);
+    console.log(repeatPassValue);
+    
+    console.log(repeatPassElement);
+    if(valid && repeatPassElement != undefined){
+        inputElements[index].classList.add("valid");
+        repeatPassElement.setCustomValidity("");
+        
+
+    } else{
+        inputElements[index].classList.add("invalid");
+        repeatPassElement.setCustomValidity("Password do not match!");
+        
+        
+    }
+
+    repeatPassElement.reportValidity();
+
+    console.log("pass is " + valid);
+    return valid;
+
 }
